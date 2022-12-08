50f7464f395150cbe4e920c1f31e5c65ef191005
Antonio Valdivia <>
2022-12-08 12:12:36 -0600

    First commit

---
+++ src/01-const-let.js
@@ -0,0 +1,15 @@
+// Variables y constantes
+
+const nombre = 'Antonio';
+const apellido = 'Valdivia';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado );
+
+if ( true ){
+    const nombre = 'Peter';
+    console.log(nombre);
+}
+
